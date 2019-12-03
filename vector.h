00651a03a0d2670ce811da2c2ed965613e1301f2
Akis Kostakis <akiskost97@outlook.com>
2019-12-04 01:12:32 +0200

    Removed exceptions,changed unsigned to size_t

--- vector.h
+++ vector.h
@@ -2,9 +2,8 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-#include<iostream>
+#include <iostream>
 #include <initializer_list>
-#include <exception>
 
 using std::cout;
 using std::cerr;
@@ -15,18 +14,15 @@ private:
 	/*An array holding all the data*/
 	T* __elem;
 	/*Size of the vector*/
-	unsigned __size;
+	size_t __size;
 	/*Current allocated size of the vector*/
-	unsigned __capacity;
+	size_t __capacity;
 public:
 	/*Empty constructor*/
 	Vector() :__elem(NULL), __size(0), __capacity(0) {}
 
 	/*Constructor given the size*/
-	Vector(unsigned cap) {
-		if (cap < 0) {
-			throw std::invalid_argument("Invalid argument");
-		}
+	Vector(size_t cap) {
 		__elem = new T[cap];
 		__size = 0;
 		__capacity = cap;
@@ -40,7 +36,7 @@ public:
 		__elem = new T[vec.size()];
 		__size = 0;
 		__capacity = vec.size();
-		for (unsigned i = 0; i < vec.size(); i++) {
+		for (size_t i = 0; i < vec.size(); i++) {
 			__elem[i] = vec[i];
 			__size++;
 		}
@@ -59,22 +55,22 @@ public:
 
 
 	/*Accessors*/
-	unsigned size() const;
+	size_t size() const;
 	bool isEmpty() const;
-	T& at(unsigned pos) const;//And Modifier
+	T& at(size_t pos) const;
 
 
 	void push_back(const T & elem);
-	void deletePos(unsigned pos);
+	void deletePos(size_t pos);
 	void clear();
 
 	/*Operators*/
-	T& operator [](unsigned pos) const;
+	T& operator [](size_t pos) const;
 
 };
 
 template<class T>
-inline unsigned Vector<T>::size() const
+inline size_t Vector<T>::size() const
 {
 	return __size;
 }
@@ -91,12 +87,8 @@ inline bool Vector<T>::isEmpty() const
 }
 
 template<class T>
-inline T & Vector<T>::at(unsigned pos) const
+inline T & Vector<T>::at(size_t pos) const
 {
-	if (pos >= __size || pos<0 ) {
-		throw std::out_of_range("Out of range exception");
-	}
-
 	return __elem[pos];
 }
 
@@ -118,12 +110,12 @@ void Vector<T>::push_back(const T & elem)
 
 	if (__size == __capacity) {
 		T* copy = new T[__capacity];
-		for (int i = 0; i < __size; i++) {
+		for (size_t i = 0; i < __size; i++) {
 			copy[i] = __elem[i];
 		}
 		__elem = new T[__capacity * 2];
 		__capacity = __capacity * 2;
-		for (unsigned i = 0; i < __size; i++) {
+		for (size_t i = 0; i < __size; i++) {
 			__elem[i] = copy[i];
 		}
 		__elem[__size] = elem;
@@ -141,16 +133,9 @@ Deletes an element from the vector and rezises it
 No Bounds checking
 */
 template<class T>
-void Vector<T>::deletePos(unsigned pos)
+void Vector<T>::deletePos(size_t pos)
 {
-	/*
-	if (pos<0 || pos>=__size) {
-		std::cerr << "Wrong position exiting" << std::endl;
-		exit(-1);
-	}
-	*/
-
-	for (unsigned i = pos; i < __size; i++) {
+	for (size_t i = pos; i < __size; i++) {
 		__elem[i] = __elem[i + 1];
 	}
 	__size--;
@@ -159,9 +144,9 @@ void Vector<T>::deletePos(unsigned pos)
 
 
 template<class T>
-inline void Vector<T>::clear()
+void Vector<T>::clear()
 {
-	for (unsigned int i = 0; i < __size; i++) {
+	for (size_t  i = 0; i < __size; i++) {
 		__elem[i] = NULL;
 	}
 	__size = 0;
@@ -170,17 +155,10 @@ inline void Vector<T>::clear()
 
 /*
 Return value of then index given
-No Bounds checking
 */
 template<class T>
-inline T & Vector<T>::operator[](unsigned pos) const
+inline T & Vector<T>::operator[](size_t pos) const
 {
-	/*
-	if (pos<0 || pos>=__capacity) {
-		cerr << "Wrong position exiting" << endl;
-		exit(-1);
-	}
-	*/
 	return __elem[pos];
 }
 
